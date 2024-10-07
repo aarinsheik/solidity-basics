c859004d9d6355bb1b6a6f7b0b0943bdefeaaa69
aarinsheik <aarinsheik.m@gmail.com>
2024-10-07 20:16:12 +0530

    initial commit

--- contracts/Counters.sol
+++ contracts/Counters.sol
@@ -20,17 +20,30 @@ contract Counter{
     address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
     // 'address' is a 20-byte Ethereum address, used to represent the address of accounts or contracts in the Ethereum blockchain.
 
-    struct student{
+    //arrays :
+    uint[] public numbers = [ 1, 2, 3 ];
+    string[] public fruits = [ 'apple' , 'banana' , 'pineapple'] ;
+    uint[][] public twoDArray = [[1,2,3],[4,5,6],[7,8,9]];
+
+    struct Student{
         uint usn ;
         string name ;
         string college ; 
     }
-    student public s1 = student(1,"aarin","BMSCE");
-    // custom datatype using structure
+    Student public s1 = Student(1,"aarin","BMSCE");     // custom datatype using structure
+
+    mapping( uint => string ) myMap;   // initialing map;  
+
+    mapping(address => mapping( uint => Student )) Colleges ;   // nested mapping
+
+    mapping( uint => Student ) Students;
 
     // visibility of constructor is public by default. No need to mention it.
     constructor() {
         count=0;
+        myMap[1] = "rakshi";
+        myMap[2] = "varshi";
+        myMap[3] = "abhi";
     }
 
     // access specifiers : public , private , internal and external.
@@ -53,6 +66,51 @@ contract Counter{
         */
         uint val = 10; //local variable
         return val;
+    }
+
+    function addNumbers( uint _val ) public {
+        numbers.push(_val);
+    }
+
+    function NumbersLength() public view returns(uint){   // view indicates that function reads state variable 'numbers', but does not modify it
+        return numbers.length;
+    }
+
+    function addFruits( string memory fr ) public {
+        fruits.push(fr);
+    }
+
+    function addStudents( uint _id , uint _usn , string memory _name , string memory _college ) public{
+        Students[_id] = Student(_usn , _name , _college);
+    }
+
+    function addColleges( uint _id , uint _usn , string memory _name , string memory _college ) public{
+        Colleges[msg.sender][_id] = Student(_usn , _name , _college);
+    }
+
+    function findEvenOrOdd(uint _index) view public returns(bool){
+        
+        if (numbers[_index] % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    
+    }
+
+    function findIndex(uint _val) view public returns(int){
+        
+        for(uint i=0 ; i<numbers.length ; i++){
+            if (numbers[i] == _val) {
+                return int(i);
+            } 
+        }
+
+        return -1;
+    }
+
+    function isOwner() view public returns(bool){
+        return(msg.sender == myAddress );
     }
 
 }
